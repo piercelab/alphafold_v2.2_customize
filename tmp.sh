2f837ef5a79995bf7e7faa80f131452888e4fcf6
Rui Yin <ryin222@terpmail.umd.edu>
2023-07-09 16:19:06 -0400

    remove unwanted files

--- tmp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bash /piercehome/yinr/antibody/antibody-antigen-complexes/af_benchmarking/af2.2/bound_template/commands_bound_H_default_ag.sh
-
-bash /piercehome/yinr/antibody/antibody-antigen-complexes/af_benchmarking/af2.2/bound_template/commands_bound_H_default_ag_ss.sh
